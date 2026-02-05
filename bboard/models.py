bdf613f7322e89064b1b64d434bac45282beacc6
Zufics2 <asaterov@gmail.com>
2026-02-05 18:25:00 +0500

    end

--- bboard/models.py
+++ bboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Bb(models.Model):
+    title = models.CharField(
+        max_length=50,
+    )
+    content = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    price = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=13,
+        decimal_places=2,
+    )
+
+    published = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+    )
