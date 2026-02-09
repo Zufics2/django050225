dac7eb92567ca5aac563235fb4d90cbb11c25d31
Сатеров Айбар <@step.kz>
2026-02-09 18:26:02 +0500

    elel

---
+++ bboard/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+class Rubric(models.Model):
+    name = models.CharField(
+        max_length=20,
+        db_index=True,
+        verbose_name='Название'
+    )
+
+    class Meta:
+        verbose_name = 'Рубрика',
+        verbose_name_plural = 'Рубрики',
+        ordering = ['name']
+
+class Bb(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Товар',
+    )
+
+    content = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание',
+    )
+
+    price = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=13,
+        decimal_places=2,
+        verbose_name='Цена',
+    )
+
+    published = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Опубликовано',
+    )
+
+    rubric = models.ForeignKey(
+        'Rubric',
+        null = True,
+        on_delete=models.PROTECT,
+        verbose_name='Рубрика',
+    )
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ['-published']
---
+++ bboard/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from django.views.generic import CreateView
+
+from bboard.forms import BbForm
+from bboard.models import Bb, Rubric
+
+
+# def index(request):
+#     s = 'Список объявлений\r\n\r\n\r\n'
+#
+#     for bb in Bb.objects.order_by('-published'):
+#         s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+#
+#     return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
+
+# def index(request):
+#     template = loader.get_template('index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#
+#     return HttpResponse(template.render(context, request))
+
+
+def index(request):
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+
+    return render(request, 'index.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = BbForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all
+        return context
