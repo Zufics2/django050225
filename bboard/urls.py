1072601d547fc7996a2e9422dd580163783583f6
Сатеров Айбар <@step.kz>
2026-03-02 18:42:36 +0500

    lesson_09 is over

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,14 +1,28 @@
 from django.urls import path
 from django.urls import re_path
 
-from bboard.views import index, by_rubric, BbCreateView
+from bboard.views import (index, by_rubric, BbCreateView, add, add_save, add_and_save, sms_list)
 
 app_name = 'bboard'
 
 urlpatterns = [
-    path('add/', BbCreateView.as_view(), name='add'),
+    # path('add/', BbCreateView.as_view(), name='add'),
+
+    # path('add/', add, name='add'),
+    # path('add/save/', add_save, name='add_save'),
+    path('add/', add_and_save, name='add'),
+
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+
     path('', index, name='index'),
+
+    #dz
+    path("sms/", sms_list, name="sms_list")
+
+    # practice
+    # path('comments/', get_comments, name="get_comments"),
+    # path('comment/out', comments_out, name="comments_out"),
+    # path('comments/delete/<int:sms_id>/', delete_comments, name="delete_comments")
 ]
 
 # url_patterns = [
--- bboard/views.py
+++ bboard/views.py
@@ -1,12 +1,33 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
+from django.template.loader import get_template, render_to_string
+from django.db.models import Count
 
 from bboard.forms import BbForm
 from bboard.models import Bb, Rubric
 
+# def index(request):
+#     resp = HttpResponse('Здесь будет', content_type='text/plain; charset=utf-8')
+#     resp.write(' главная')
+#     resp.writelines((' страница', ' сайта'))
+#     resp['keywords'] = 'Python, Django'
+#     return resp
+
+# def index(request):
+#     bbs = Bb.objects.order_by('-published')
+#     rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+#     context = {'bbs' : bbs, 'rubrics': rubrics}
+#     template = get_template('index.html')
+#     return HttpResponse(template.render(context, request))
+
+def index(request):
+    bbs = Bb.objects.order_by('-published')
+    rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+    context = {'bbs' : bbs, 'rubrics': rubrics}
+    return HttpResponse(render_to_string('index.html', context, request))
 
 # def index(request):
 #     s = 'Список объявлений\r\n\r\n\r\n'
@@ -25,12 +46,12 @@ from bboard.models import Bb, Rubric
 #     return HttpResponse(template.render(context, request))
 
 
-def index(request):
-    bbs = Bb.objects.order_by('-published')
-    rubrics = Rubric.objects.all()
-    context = {'bbs': bbs, 'rubrics': rubrics}
-
-    return render(request, 'index.html', context)
+# def index(request):
+#     bbs = Bb.objects.order_by('-published')
+#     rubrics = Rubric.objects.all()
+#     context = {'bbs': bbs, 'rubrics': rubrics}
+#
+#     return render(request, 'index.html', context)
 
 
 def by_rubric(request, rubric_id):
@@ -55,3 +76,50 @@ class BbCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
         return context
+
+def add(request):
+    bbf = BbForm()
+    context = {'form': bbf}
+    return render(request, 'create.html', context)
+
+def add_save(request):
+    bbf = BbForm(request.POST)
+    if bbf.is_valid():
+        bbf.save()
+        return HttpResponseRedirect(reverse('bboard:by_rubric',
+                                            kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+    else:
+        context = {'form': bbf}
+        return render(request, 'create.html', context)
+
+def add_and_save(request):
+    if request.method == 'POST':
+        bbf = BbForm(request.POST)
+        if bbf.is_valid():
+            bbf.save()
+            return HttpResponseRedirect(reverse('bboard:by_rubric',
+                                                kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+        else:
+            context = {'form': bbf}
+            return render(request, 'create.html', context)
+    else:
+        bbf = BbForm()
+        context = {'form': bbf}
+        return render(request, 'create.html', context)
+
+#PR
+# def index(request):
+#     return HttpResponse('Здесь был текст 1 задания', content_type='text/plain; charset=utf-8')
+
+# def index(request):
+#     text_html = """
+#         <h1>Тут </h1>
+#         <i>уже </i>
+#         <b>второе задание</b>
+#         """
+#     return HttpResponse(text_html)
+
+#dz
+def sms_list(request):
+    sms_list = SMS.objects.all().order_by("-id")
+    return render(request, "templates/sms_list.html", {"sms_list": sms_list})
